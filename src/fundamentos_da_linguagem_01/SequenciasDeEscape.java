bdf426ec3b23264a739fa014046535b6a454a966
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 10:45:19 -0300

    Sequencia de Escape

--- src/fundamentos_da_linguagem_01/SequenciasDeEscape.java
+++ src/fundamentos_da_linguagem_01/SequenciasDeEscape.java
@@ -2,6 +2,10 @@ package fundamentos_da_linguagem_01;
 
 public class SequenciasDeEscape {
     public static void main(String[] args) {
+        //seguencia de escape para mostrar -> " "
         System.out.println("Oi \"Mundo!!\"");
+
+        // \n pula uma linha
+        System.out.println("Oi \nMundo!!");
     }
 }
