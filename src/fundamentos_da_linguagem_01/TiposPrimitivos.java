32793d1ae8c5af3ff7a5396af4806268ca153b1d
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 13:35:08 -0300

    Tipos Primitivos

---
+++ src/fundamentos_da_linguagem_01/TiposPrimitivos.java
@@ -0,0 +1,21 @@
+package fundamentos_da_linguagem_01;
+
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+
+        double precoProduto = 9.43; // ponto e não virgula
+        boolean alunoMatriculado = false;
+
+        char turma = 'A';
+        char simbolo = '@';
+
+        long populacaoUberlandia = 650000;
+        System.out.println(populacaoUberlandia);
+
+        long populacaoMundial = 7000000000L; // e um literal do tipo long
+
+        float saldoDaConta = 1030.43F;
+        System.out.println(saldoDaConta);
+
+    }
+}
