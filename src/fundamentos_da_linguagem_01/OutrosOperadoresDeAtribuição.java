3db5aebc3a146d9d2621299e34e1c7c17d9f417a
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 13:39:31 -0300

    Outros operadores de Atribuição

---
+++ src/fundamentos_da_linguagem_01/OutrosOperadoresDeAtribuição.java
@@ -0,0 +1,20 @@
+package fundamentos_da_linguagem_01;
+
+public class OutrosOperadoresDeAtribuição {
+    public static void main(String[] args) {
+
+        int total = 10;
+
+        //total = total + 3;
+        total += + 3;
+        System.out.println(total);
+        total -= + 3;
+        System.out.println(total);
+        total *= + 3;
+        System.out.println(total);
+        total /= + 3;
+        System.out.println(total);
+        total %= + 3;
+        System.out.println(total);
+    }
+}
