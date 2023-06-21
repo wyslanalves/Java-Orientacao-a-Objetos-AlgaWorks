f36e7d2f72332f9aa66051a7795f9b63cf40ee5d
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 10:52:18 -0300

    Palavras Reservadas

---
+++ src/fundamentos_da_linguagem_01/PalavrasReservadas.java
@@ -0,0 +1,23 @@
+package fundamentos_da_linguagem_01;
+
+public class PalavrasReservadas {
+    public static void main(String[] args) {
+
+        /*
+        essass são algumas palavras reservadas ->
+
+        public return
+        short  static
+        assert enum
+        super  void
+        try    while
+        volatile try
+        switch strictfp
+         */
+
+        System.out.println("Teste de palavra reservada");
+
+        //int void = 10 -> erro
+        int valor = 10; // correto
+    }
+}
