529a30b6973d2294d5130cfb8a45310afcbad2e5
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 17:25:48 -0300

    Promoção Aritmética

---
+++ src/fundamentos_da_linguagem_01/PromocaoAritmetica.java
@@ -0,0 +1,19 @@
+package fundamentos_da_linguagem_01;
+
+public class PromocaoAritmetica {
+    public static void main(String[] args) {
+        /*
+        int x = 10;
+        long y = 5;
+        //int z = x + y; // não compila, tem que mudar a variavel z em LONG
+        long z = x + y;
+        System.out.println(z);
+        */
+        long x = 10;
+        float y = 9.34f;
+        //long z = x + y; // não compila, tem que mudar a variavel z em LONG
+        float z = x + y;
+        System.out.println(z);
+
+    }
+}
