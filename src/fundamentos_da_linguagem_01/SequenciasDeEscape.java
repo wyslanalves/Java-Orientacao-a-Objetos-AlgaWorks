34f8525c49d654105734a9cfbab8a4d122c08590
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 13:04:19 -0300

    Trabalhando com Variáveis

--- src/fundamentos_da_linguagem_01/SequenciasDeEscape.java
+++ src/fundamentos_da_linguagem_01/SequenciasDeEscape.java
@@ -2,10 +2,14 @@ package fundamentos_da_linguagem_01;
 
 public class SequenciasDeEscape {
     public static void main(String[] args) {
-        //seguencia de escape para mostrar -> " "
-        System.out.println("Oi \"Mundo!!\"");
 
-        // \n pula uma linha
-        System.out.println("Oi \nMundo!!");
+        int quantidade; //declarando a variavel inteiro
+        quantidade = 10; // aqui estou atribuindo o valor 10
+
+        System.out.println(quantidade);
+
+        quantidade = 15; // trocando o valor
+
+        System.out.println(quantidade);
     }
 }
---
+++ src/fundamentos_da_linguagem_01/TrabalhandoComVariaveis.java
@@ -0,0 +1,10 @@
+package fundamentos_da_linguagem_01;
+
+public class TrabalhandoComVariaveis {
+    public static void main(String[] args) {
+
+        int numero = 15;
+        System.out.println(numero);
+
+    }
+}
