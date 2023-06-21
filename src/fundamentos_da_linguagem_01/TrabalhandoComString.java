5b45d31ebce32a250ae7dacc990a5708658aa904
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 17:32:34 -0300

    Trabalhando com String

---
+++ src/fundamentos_da_linguagem_01/TrabalhandoComString.java
@@ -0,0 +1,20 @@
+package fundamentos_da_linguagem_01;
+
+public class TrabalhandoComString {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 5;
+        int z = x + y;
+        System.out.println("Oi meu amigo do bem!! " + z);
+
+        int a = 10;
+        int b = 5;
+        System.out.println("Oi meu amigo do bem!! " + a + b);//
+
+        String nome = "Maria";
+        int idade = 30;
+        System.out.println(nome + " tem " + idade + " anos ");
+
+    }
+}
