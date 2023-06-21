91760ede0a1a43a771c8b9ddb3cc84fa16716b2a
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 10:41:49 -0300

    Criando o Projeto

---
+++ src/fundamentos_da_linguagem_01/Comentario.java
@@ -0,0 +1,12 @@
+package fundamentos_da_linguagem_01;
+
+public class Comentario {
+    public static void main(String[] args) {
+        // -> A linha abaixo encia uma mensagem
+        /*
+        * essa linha sera ignorada pelo compilador
+        *System.out.println("essa instrucao sera ignorada tambem");
+        * */
+        System.out.println("Oi Mundo");
+    }
+}
---
+++ src/fundamentos_da_linguagem_01/OiMundo.java
@@ -0,0 +1,7 @@
+package fundamentos_da_linguagem_01;
+
+public class OiMundo {
+    public static void main(String[] args) {
+        System.out.println("Oi Mundo!!");
+    }
+}
