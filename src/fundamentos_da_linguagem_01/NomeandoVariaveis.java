9ebc9827502cf9304fddc0c0727ec65084ea8abb
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-21 13:14:39 -0300

    Nomeando Variáveis

---
+++ src/fundamentos_da_linguagem_01/NomeandoVariaveis.java
@@ -0,0 +1,26 @@
+package fundamentos_da_linguagem_01;
+
+public class NomeandoVariaveis {
+    public static void main(String[] args) {
+
+        //melhor forma de declarar variaveis
+        int quantidadeDeAlunos;
+        int quantidadeDeAlunosNota10;
+        int 
+
+        int quantidade;
+        int quantidade_aluno;
+        int QUANTIDADE;
+        int AuantidadeAlunos;
+        int $quantidade;
+        int _quantidade;
+        int quantidade_alunos_nota_10;
+
+        // Nomes de variaveis que são errado
+
+        //int 1quantidade;
+        //int quantidade alunos
+
+
+    }
+}
--- src/fundamentos_da_linguagem_01/TrabalhandoComVariaveis.java
+++ src/fundamentos_da_linguagem_01/TrabalhandoComVariaveis.java
@@ -3,8 +3,14 @@ package fundamentos_da_linguagem_01;
 public class TrabalhandoComVariaveis {
     public static void main(String[] args) {
 
-        int numero = 15;
-        System.out.println(numero);
+        int quantidade; //declarando a variavel inteiro
+        quantidade = 10; // aqui estou atribuindo o valor 10
+
+        System.out.println(quantidade);
+
+        quantidade = 15; // trocando o valor
+
+        System.out.println(quantidade);
 
     }
 }
