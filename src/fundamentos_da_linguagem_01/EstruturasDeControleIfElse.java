6119a1b7215f1ed553ded115cc407c709bd4cd0e
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-22 20:21:51 -0300

    Estrutura de Controle If, Else If e Else

---
+++ src/fundamentos_da_linguagem_01/EstruturasDeControleIfElse.java
@@ -0,0 +1,35 @@
+package fundamentos_da_linguagem_01;
+
+import java.util.Scanner;
+
+public class EstruturasDeControleIfElse {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Nome: ");
+        String nome = entrada.nextLine();
+
+        System.out.print("Peso :");
+        int peso = entrada.nextInt();
+
+        System.out.print("Altura: ");
+        double altura = entrada.nextDouble();
+
+        double imc = peso / (altura * altura);
+        
+
+        System.out.println("IMC de " + nome + "; " + imc);
+
+        if(imc < 18.5){
+            System.out.println("Abaixo do peso Ideal");
+        }else if(imc < 25){
+            System.out.println("Peso Ideal");
+        }else if(imc < 30){
+            System.out.println("Acima do peso");
+        }else  if(imc < 40){
+            System.out.println("Obesidade grau 1 ou 2");
+        }else {
+            System.out.println("Obesidade grau 3");
+        }
+    }
+}
