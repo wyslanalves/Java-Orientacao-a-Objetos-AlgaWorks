8bd126591e190abb2ee3cb06e15825cb4280ac88
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-24 20:48:22 -0300

    Cláusulas Break e Continue

--- src/fundamentos_da_linguagem_01/ClausulasBreakContinue.java
+++ src/fundamentos_da_linguagem_01/ClausulasBreakContinue.java
@@ -5,13 +5,37 @@ import java.util.Scanner;
 public class ClausulasBreakContinue {
     public static void main(String[] args) {
 
+        /*
         Scanner entrada = new Scanner(System.in);
 
-        System.out.println("Ultimo numero: ");
-        int numeroFinal = entrada.nextInt();
+        System.out.println("Digite um Numero: ");
+        int divisor = entrada.nextInt();
 
-        for(int i = 1; i <= numeroFinal; i++){
+        for(int i = 100; i <= 200; i++){
+            if(i % divisor == 0){
+                break;
+            }
+
             System.out.println(i);
         }
+
+        System.out.println("Fim do Programa");
     }
+
+         */
+
+    Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um Numero: ");
+        int divisor = entrada.nextInt();
+
+        for(int i = 100; i <= 120; i++){
+        if(i % divisor == 0){
+            continue;
+            }
+            System.out.println(i);
+        }
+
+        System.out.println("Fim do Programa");
+}
 }
