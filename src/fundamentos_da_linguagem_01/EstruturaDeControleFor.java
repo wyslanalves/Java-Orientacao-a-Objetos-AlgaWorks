8776170f5e9a7f3649bdd56d5c017a1f0276eaef
wyslan alves <wyslandasilvaalves@gmail.com>
2023-06-24 17:03:10 -0300

    Estrutura de Controler For

---
+++ src/fundamentos_da_linguagem_01/EstruturaDeControleFor.java
@@ -0,0 +1,17 @@
+package fundamentos_da_linguagem_01;
+
+import java.util.Scanner;
+
+public class EstruturaDeControleFor {
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Ultimo numero: ");
+        int numeroFinal = entrada.nextInt();
+
+        for(int i = 1; i <= numeroFinal; i++){
+            System.out.println(i);
+        }
+    }
+}
